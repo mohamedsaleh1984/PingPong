e0e57a2bdd95d78ad0f29608fa79d134b0a967d6
mohamed.saleh <mohamedsaleh1984@hotmail.com>
2024-10-24 22:54:21 -0400

    Refactor and clean up

--- PingPong/game.cpp
+++ PingPong/game.cpp
@@ -30,7 +30,32 @@ static bool IsReleased(Input* input, enumButtons button) {
 	return !(input->buttons[button].isDown && input->buttons[button].changed);
 }
 
+static void simulatePlayer(float* position, float* velocity, float acceleration, float delta) {
 
+	// tweak the speed a little
+	acceleration -= (*velocity * ACCELERATION_BREAKER_FACTOR);
+
+	// Calculate New Position = CurrentPosition + V*DeltaT + (A*DeltaT*DeltaT)/2
+	*position = *position + (*velocity * delta) + (acceleration * delta * delta) / 2.0f;
+	*velocity = *velocity + acceleration * delta;
+
+	// collide to Top Side
+	if (*position + playerHalfSizeY > arenaHalfSizeY) {
+		//Stop the Player
+		*position = arenaHalfSizeY - playerHalfSizeY;
+
+		// Bounce back in different direction
+		// velocity *= -1;
+
+		// come to rest
+		*velocity = 0;
+	} // collide to bottom side
+	else if (*position - playerHalfSizeY < -arenaHalfSizeY) {
+		*position = -arenaHalfSizeY + playerHalfSizeY;
+		*velocity = 0;
+	}
+
+}
 
 static void simulateGame(Input* input, float delta) {
 	clearScreen(0xff5500);
@@ -49,28 +74,8 @@ static void simulateGame(Input* input, float delta) {
 		player1_acceleration -= ACCELERATION_FACTOR;
 	}
 
-	// tweak the speed a little
-	player1_acceleration -= (player1_velocity * ACCELERATION_BREAKER_FACTOR);
+	simulatePlayer(&player1_pos, &player1_velocity, player1_acceleration, delta);
 
-	// Calculate New Position = CurrentPosition + V*DeltaT + (A*DeltaT*DeltaT)/2
-	player1_pos = player1_pos + (player1_velocity * delta) + (player1_acceleration * delta * delta) / 2.0f;
-	player1_velocity = player1_velocity + player1_acceleration * delta;
-
-	// collide to Top Side
-	if (player1_pos + playerHalfSizeY > arenaHalfSizeY) {
-		//Stop the Player
-		player1_pos = arenaHalfSizeY - playerHalfSizeY;
-
-		// Bounce back in different direction
-		// player1_velocity *= -1;
-
-		// come to rest
-		player1_velocity = 0;
-	} // collide to bottom side
-	else if (player1_pos - playerHalfSizeY < -arenaHalfSizeY) {
-		player1_pos = -arenaHalfSizeY + playerHalfSizeY;
-		player1_velocity = 0;
-	}
 
 
 	/*******************************************************************************************/
@@ -84,104 +89,86 @@ static void simulateGame(Input* input, float delta) {
 		player2_acceleration -= ACCELERATION_FACTOR;
 	}
 
-	// tweak the speed a little
-	player2_acceleration -= (player2_velocity * ACCELERATION_BREAKER_FACTOR);
-	player2_pos = player2_pos + (player2_velocity * delta) + (player2_acceleration * delta * delta) / 2.0f;
-	player2_velocity = player2_velocity + player2_acceleration * delta;
-
-	// collide to Top Side
-	if (player2_pos + playerHalfSizeY > arenaHalfSizeY) {
-		//Stop the Player
-		player2_pos = arenaHalfSizeY - playerHalfSizeY;
-
-		// Bounce back in different direction
-		// player1_velocity *= -1;
-
-		// come to rest
-		player2_velocity = 0;
-	} // collide to bottom side
-	else if (player2_pos - playerHalfSizeY < -arenaHalfSizeY) {
-		player2_pos = -arenaHalfSizeY + playerHalfSizeY;
-		player2_velocity = 0;
-	}
-
+	simulatePlayer(&player2_pos, &player2_velocity, player2_acceleration, delta);
 
 	/*******************************************************************************************/
 	// Ball
-	//move towards player 1
-	ball_posX += ball_velX * delta;
-	ball_posY += ball_velY * delta;
-
-	// Ball Collision on the right Player
-	if (ball_posX + ballHalfSize > 80 - playerHalfSizeX &&
-		ball_posX - ballHalfSize < 80 + playerHalfSizeX &&
-		ball_posY + ballHalfSize > player1_pos - playerHalfSizeY &&
-		ball_posY + ballHalfSize < player1_pos + playerHalfSizeY)
 	{
-		// Change the ball x direction to the opposite side
-		ball_posX = 80 - playerHalfSizeX - ballHalfSize;
-		ball_velX *= -1;
+		//move towards player 1
+		ball_posX += ball_velX * delta;
+		ball_posY += ball_velY * delta;
 
-		// Change Y velocity according to the player velocity
-		// ball_velY = player1_velocity * ball_velocity_coef;
+		// Ball Collision on the right Player
+		if (ball_posX + ballHalfSize > 80 - playerHalfSizeX &&
+			ball_posX - ballHalfSize < 80 + playerHalfSizeX &&
+			ball_posY + ballHalfSize > player1_pos - playerHalfSizeY &&
+			ball_posY + ballHalfSize < player1_pos + playerHalfSizeY)
+		{
+			// Change the ball x direction to the opposite side
+			ball_posX = 80 - playerHalfSizeX - ballHalfSize;
+			ball_velX *= -1;
 
-		// Make the ball go up or down depends on where ball hit the player
-		// Depends on how far the ball center to the player
-		// ball_velY = (ball_posY - player1_pos) * ball_velocity_coef;
+			// Change Y velocity according to the player velocity
+			// ball_velY = player1_velocity * ball_velocity_coef;
 
-		// Depends on how far the ball center to the player  AND Player Velocity
-		ball_velY = (ball_posY - player1_pos) * ball_velocity_coef + player1_velocity * ball_velocity_coef;
+			// Make the ball go up or down depends on where ball hit the player
+			// Depends on how far the ball center to the player
+			// ball_velY = (ball_posY - player1_pos) * ball_velocity_coef;
+
+			// Depends on how far the ball center to the player  AND Player Velocity
+			ball_velY = (ball_posY - player1_pos) * ball_velocity_coef + player1_velocity * ball_velocity_coef;
+		}
+
+		// Ball Collision on the left Player
+		if (ball_posX + ballHalfSize > -80 - playerHalfSizeX &&
+			ball_posX - ballHalfSize < -80 + playerHalfSizeX &&
+			ball_posY + ballHalfSize > player2_pos - playerHalfSizeY &&
+			ball_posY + ballHalfSize < player2_pos + playerHalfSizeY)
+		{
+			ball_posX = -80 + playerHalfSizeX + ballHalfSize;
+			ball_velX *= -1;
+
+			// Depends on how far the ball center to the player  AND Player Velocity
+			ball_velY = (ball_posY - player2_pos) * ball_velocity_coef + player2_velocity * ball_velocity_coef;
+		}
+
+
+		// Ball Collision to the Top Wall
+		if (ball_posY + ballHalfSize > arenaHalfSizeY) {
+			ball_posY = arenaHalfSizeY - ballHalfSize;
+			ball_velY *= -1;
+		}
+
+		// Ball Collision to the Bottom Wall
+		if (ball_posY - ballHalfSize < -arenaHalfSizeY) {
+			ball_posY = -arenaHalfSizeY + ballHalfSize;
+			ball_velY *= -1;
+		}
+
+
+		// Reset Ball and give it to opposite player
+		if (ball_posX + ballHalfSize > arenaHalfSizeX) {
+			ball_velX *= -1;
+			ball_velY = 0;
+			ball_posX = 0;
+			ball_posY = 0;
+		}
+
+		if (ball_posX + ballHalfSize < -arenaHalfSizeX) {
+			ball_velX *= -1;
+			ball_velY = 0;
+			ball_posX = 0;
+			ball_posY = 0;
+		}
+
 	}
-
-	// Ball Collision on the left Player
-	if (ball_posX + ballHalfSize > -80 - playerHalfSizeX &&
-		ball_posX - ballHalfSize < -80 + playerHalfSizeX &&
-		ball_posY + ballHalfSize > player2_pos - playerHalfSizeY &&
-		ball_posY + ballHalfSize < player2_pos + playerHalfSizeY)
-	{
-		ball_posX = -80 + playerHalfSizeX + ballHalfSize;
-		ball_velX *= -1;
-
-		// Depends on how far the ball center to the player  AND Player Velocity
-		ball_velY = (ball_posY - player2_pos) * ball_velocity_coef + player2_velocity * ball_velocity_coef;
-	}
-
-
-	// Ball Collision to the Top Wall
-	if (ball_posY + ballHalfSize > arenaHalfSizeY) {
-		ball_posY = arenaHalfSizeY - ballHalfSize;
-		ball_velY *= -1;
-	}
-
-	// Ball Collision to the Bottom Wall
-	if (ball_posY - ballHalfSize < -arenaHalfSizeY) {
-		ball_posY = -arenaHalfSizeY + ballHalfSize;
-		ball_velY *= -1;
-	}
-
-
-	// Reset Ball and give it to opposite player
-	if (ball_posX + ballHalfSize > arenaHalfSizeX) {
-		ball_velX *= -1;
-		ball_velY = 0;
-		ball_posX = 0;
-		ball_posY = 0;
-	}
-
-	if (ball_posX + ballHalfSize < -arenaHalfSizeX) {
-		ball_velX *= -1;
-		ball_velY = 0;
-		ball_posX = 0;
-		ball_posY = 0;
-	}
-
 
 	// Draw the ball
 	drawRect(ball_posX, ball_posY, ballHalfSize, ballHalfSize, 0x000000);
 
-
 	// Right Player
 	drawRect(80, player1_pos, playerHalfSizeX, playerHalfSizeY, 0xffC0A0);
+
 	// Left Player
 	drawRect(-80, player2_pos, playerHalfSizeX, playerHalfSizeY, 0xff0022);
 }
