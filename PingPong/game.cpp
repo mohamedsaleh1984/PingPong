fd16a8ce0283703b4d70a52350c2a8d2550dcbca
mohamed.saleh <mohamedsaleh1984@hotmail.com>
2024-10-24 20:53:56 -0400

    Adding Velocity and Acceleration to Player 1

--- PingPong/game.cpp
+++ PingPong/game.cpp
@@ -1,4 +1,4 @@
-bool IsDown(Input* input,enumButtons button) {
+bool IsDown(Input* input, enumButtons button) {
 	return input->buttons[button].isDown;
 }
 bool IsPressed(Input* input, enumButtons button) {
@@ -9,22 +9,29 @@ bool IsReleased(Input* input, enumButtons button) {
 }
 
 float player_pos = 0.f;
+float player_velocity = 0.f;
 
 internal void simulateGame(Input* input, float delta) {
 	clearScreen(0xff5500);
-
 	// arena
 	drawRect(0, 0, 85, 45, 0xffaa33);
 
-	float speed = 50.0f;
+	float player_acceleration = 0.f;
 
-	if (IsDown(input,BUTTON_UP)) {
-		player_pos += speed * delta;
+	if (IsDown(input, BUTTON_UP)) {
+		player_acceleration += 2000;
 	}
 
 	if (IsDown(input, BUTTON_DOWN)) {
-		player_pos -= speed * delta;
+		player_acceleration -= 2000;
 	}
+
+	// tweak the speed a little
+	player_acceleration -= (player_velocity * 10.f);
+
+	//New Position = CurrentPosition + V*DeltaT + (A*DeltaT*DeltaT)/2
+	player_pos = player_pos + (player_velocity * delta) + (player_acceleration * delta * delta) / 2.0f;
+	player_velocity = player_velocity + player_acceleration * delta;
 
 
 	drawRect(0, 0, 1, 1, 0x000000);
